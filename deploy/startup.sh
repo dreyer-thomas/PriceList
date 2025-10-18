be8bb063ce514afe342184122377fe4d09b8896b
Thomas Dreyer <thomas-dreyer@gmx.de>
2025-10-18 13:02:35 +0200

    Scripts corrected (1)

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -9,20 +9,18 @@ if ping -c 1 -W 1 github.com >/dev/null 2>&1; then
   echo "Netz verfügbar, aktualisiere Repository" >> /home/admin/startup.log
   git reset --hard
   git pull origin main
+  cp /home/admin/PriceList/deploy/backup.sh /home/admin/backup.sh
+  chmod +x /home/admin/backup.sh
+  cp /home/admin/PriceList/deploy/startup.sh /home/admin/startup.sh
+  chmod +x /home/admin/startup.sh
   cd server
   npm install
   cd ..
   npm install
   rm -rd /home/admin/backup/*
-  cp -r server/public/browser/data /home/admin/backup
-  cp -r server/public/browser/images /home/admin/backup
+  /home/admin/backup backup
   npm run build
-  cp -r /home/admin/backup/data server/public/browser/data
-  cp -r /home/admin/backup/images server/public/browser/images
-  cp /home/admin/PriceList/deploy/backup.sh /home/admin/backup.sh
-  chmod +x /home/admin/backup.sh
-  cp /home/admin/PriceList/deploy/startup.sh /home/admin/startup.sh
-  chmod +x /home/admin/startup.sh
+  /home/admin/backup restore
   git pull >> /home/admin/startup.log 2>&1
 else
   echo "Kein Internet – überspringe git pull" >> /home/admin/startup.log
